b7fb6ee68ffb9cee9291af5c1d4655a2124077d9
Antonio Prcela <antonio.prcela@sartura.hr>
2024-07-12 15:24:59 +0200

    doc(Copyright): update Copyright

--- plugins/ietf-system-plugin/tests/unit/system_utest.c
+++ plugins/ietf-system-plugin/tests/unit/system_utest.c
@@ -5,8 +5,8 @@
  * BSD 3-Clause license which is available at
  * https://opensource.org/licenses/BSD-3-Clause
  *
- * SPDX-FileCopyrightText: 2022 Deutsche Telekom AG
- * SPDX-FileContributor: Sartura Ltd.
+ * SPDX-FileCopyrightText: 2024 Deutsche Telekom AG
+ * SPDX-FileContributor: Sartura d.d.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
