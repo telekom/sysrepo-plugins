74e042ca86e68e72bf8e91a01fe0a29b39284132
Antonio Prcela <antonio.prcela@sartura.hr>
2026-04-07 10:20:40 +0200

    feat: enable -Wunintialized
    Signed-off-by: Antonio Prcela <antonio.prcela@sartura.hr>

--- CompileOptions.cmake
+++ CompileOptions.cmake
@@ -23,7 +23,7 @@ if(CMAKE_C_COMPILER_ID MATCHES "Clang")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-newline-eof")
 endif()
 # remove some warnings that occur too often and are not straight forward or code is not finished, like unused functions
-# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wuninitialized")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wuninitialized")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wshadow")
 # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wconversion")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-delete-non-virtual-dtor")
